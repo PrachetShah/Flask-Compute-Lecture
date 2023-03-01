9086f7c4f0b3dae503c2ca353aa574f3fb5e4e9a
PrachetShah <prachetshah25@gmail.com>
2023-03-01 22:55:19 +0530

    part1 done

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/secure")
+def secure():
+    # to prevent this we use escape() function
+    return '<script>alert("bad")</script>'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    # app.run()
